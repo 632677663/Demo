58428b93082ce29ed0907d69beb1479030d3b3c1
jack <632677663@qq.com>
2016-05-25 17:47:58 +0800

    添加.

---
+++ scorpio-manager/scorpio-manager-dao/src/main/java/com/scorpio/dao/TItemDao.java
@@ -0,0 +1,30 @@
+package com.scorpio.dao;
+
+import com.scorpio.bean.TItem;
+import com.scorpio.bean.TItemExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TItemDao {
+    int countByExample(TItemExample example);
+
+    int deleteByExample(TItemExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TItem record);
+
+    int insertSelective(TItem record);
+
+    List<TItem> selectByExample(TItemExample example);
+
+    TItem selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TItem record, @Param("example") TItemExample example);
+
+    int updateByExample(@Param("record") TItem record, @Param("example") TItemExample example);
+
+    int updateByPrimaryKeySelective(TItem record);
+
+    int updateByPrimaryKey(TItem record);
+}
