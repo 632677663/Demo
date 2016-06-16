318b97ad7d157d79d5da160834024e442f43d755
jack <632677663@qq.com>
2016-06-16 17:53:05 +0800

    添加代码

--- scorpio-utils/src/main/java/com/scorpio/utils/IDUtils.java
+++ scorpio-utils/src/main/java/com/scorpio/utils/IDUtils.java
@@ -29,7 +29,6 @@ public class IDUtils {
 	public static long genItemId() {
 		//取当前时间的长整形值包含毫秒
 		long millis = System.currentTimeMillis();
-		//long millis = System.nanoTime();
 		//加上两位随机数
 		Random random = new Random();
 		int end2 = random.nextInt(99);
